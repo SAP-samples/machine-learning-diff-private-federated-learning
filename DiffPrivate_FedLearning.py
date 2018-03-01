36e66e54149aad5a3207b4ac26922cf5c340704c
cyrusgeyer <cyrusgeyer@gmail.com>
2018-03-01 10:08:12 +0100

    Update on the function's doc-string
    The core function that runs federated learning with diff. privacy

--- DiffPrivate_FedLearning.py
+++ DiffPrivate_FedLearning.py
@@ -24,12 +24,11 @@ def run_differentially_private_federated_averaging(loss, train_op, eval_correct,
 
     with tf.Graph().as_default():
 
-        train_op, eval_correct, loss = Some_function_that_builds_the_TF_graph()
+        train_op, eval_correct, loss, data_placeholder, labels_placeholder = Some_function_that builds_TF_graph()
 
         Accuracy_accountant, Delta_accountant, model = \
-            run_differentially_private_federated_averaging(loss, train_op, eval_correct,
-                                                           DATA, data_placeholder, labels_placeholder)
-
+            run_differentially_private_federated_averaging(loss, train_op, eval_correct, DATA, data_placeholder,
+                                                           labels_placeholder)
     --------------------------------------------------------------------------------------------------------------------
     The graph that train_op, loss and eval_op belong to should have a global_step variable.
 
